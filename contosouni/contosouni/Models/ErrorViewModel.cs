7a1be0445720ab60f8c1da6022c58e7072fd727a
LAUREN\lauren <laurenpnn@gmail.com>
2024-08-26 11:36:49 +0300

    ll

---
+++ contosouni/contosouni/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace contosouni.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
